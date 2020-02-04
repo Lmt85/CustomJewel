e9bac01b793f69c6503621d9dccfd8756cf5524b
Luis Mario <luis.mario.trujillo@live.com.mx>
2020-02-04 00:32:53 -0600

    Created new pages

---
+++ src/screens/RingColor.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+function RingColor() {
+  return (
+    <div className="App">
+        <h1>Ring Color</h1>
+    </div>
+    );
+}
+
+export default RingColor;
