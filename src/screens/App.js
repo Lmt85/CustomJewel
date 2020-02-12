497685efea174951bb88983e17317a0a2e8036a5
Luis Mario <luis.mario.trujillo@live.com.mx>
2020-02-11 20:11:04 -0600

    Completed routing

--- src/screens/App.js
+++ src/screens/App.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import './App.css';
-import Dashboard from '../components/dashboard/Dashboard'; 
+import Nav from './Nav';
+import RingColor from './RingColor';
+import RingDetail from './RingDetail';
+import Dashboard from '../components/dashboard/Dashboard';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 function App() {
   return (
-    <Dashboard  />
+    <Router> 
+      <div className="App">
+        <Nav />
+        <Switch>
+        <Route path="/" exact component={Home}/>
+        <Route path="/RingColor" component={RingColor}/>
+        <Route path="/RingDetail" component={RingDetail}/>
+        </Switch>
+      </div>
+    </Router>    
   );
 }
+
+const Home = () => (
+  <div>
+    < Dashboard />
+  </div>
+);
 
 export default App;
